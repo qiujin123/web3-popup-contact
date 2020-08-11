07663ee4b2a0ee924ca1423d2ec9ecd75bc83a2d
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2020-08-11 15:48:49 -0400

    kinda animation inside the form but it's weird

--- dev/js/cd-animation.js
+++ dev/js/cd-animation.js
@@ -1,4 +1,27 @@
-var formAnimationSpeed = 0.5;
 var formAnimation = gsap.timeline();
-gsap.set(".input",{transformOrigin:"right center"});
-formAnimation.from(".input",{duration: formAnimationSpeed, alpha: 0, x: 100}, "inputAnimation");
+
+gsap.registerPlugin(MotionPathPlugin);
+
+gsap.set(".input",{transformOrigin: "left center", alpha:1});
+
+let myPath = document.querySelector("#input-path");
+
+formAnimation.from(".main-text",{
+    duration:1,
+    y:-100,
+    alpha:0,
+    stagger:0.1
+}, "formAnimation")
+
+.from(".input", {
+    motionPath: {
+        path: myPath,
+        autoRotate: true
+    },
+    duration: 1.5,
+    ease: "power1.inOut",
+    alpha:0,
+    borderBottom:"2px transparent",
+    stagger:0.1
+}, "formAnimation")
+;
